8e22c0f7019117f5f1c6be9d17510350cbad2c4f
Chris Aga <chrisaga.dev@gmail.com>
2020-08-01 18:07:07 -0500

    First attempt

--- libs/nesting.rb
+++ libs/nesting.rb
@@ -21,6 +21,40 @@ class Nesting
   # @param {String} text
   # @return {Boolean}
   def check_if_properly_nested(text)
-    nil
+    open_characters = {
+      "(": :")",
+      "{": :"}",
+      "[": :"]",
+    }
+    close_characters = open_characters.invert
+    current_open_characters = []
+
+    text_symbols = text.chars.map(&:to_sym)
+    text_symbols.each.with_index do |character, index|
+      is_close_character = close_characters.has_key?(character)
+      is_open_character = open_characters.has_key?(character)
+
+      if is_close_character
+        matching_open_character = close_characters[character]
+        last_open_character = current_open_characters.pop
+        if last_open_character != matching_open_character
+          return false
+        end
+      elsif is_open_character
+        if index == text.size - 1
+          return false
+        end
+        current_open_characters.push(character)
+      else
+        return false
+      end
+    end
+
+
+=begin
+    if closing_character
+      last_open_character = open_characters.pop
+      return false if last_open_character != closing_characters[last_open_character]
+=end
   end
 end
