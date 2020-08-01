85d68b2e7dcc262fbda3a25109acff4fec3d2fbb
Chris Aga <chrisaga.dev@gmail.com>
2020-08-01 18:23:25 -0500

    Got slightly closer

--- libs/nesting.rb
+++ libs/nesting.rb
@@ -27,7 +27,11 @@ class Nesting
       "[": :"]",
     }
     close_characters = open_characters.invert
-    current_open_characters = []
+
+    open_count = open_characters.keys.map { |x| [x, 0] }.to_h
+    close_count = close_characters.keys.map { |x| [x, 0] }.to_h
+
+    open_stack = []
 
     text_symbols = text.chars.map(&:to_sym)
     text_symbols.each.with_index do |character, index|
@@ -36,18 +40,41 @@ class Nesting
 
       if is_close_character
         matching_open_character = close_characters[character]
-        last_open_character = current_open_characters.pop
+        last_open_character = open_stack.pop
+        if last_open_character.nil?
+          puts "false 1"
+          return false
+        end
+        begin
+          open_count[last_open_character] -= 1
+          
+        rescue => exception
+          puts "open_count: #{open_count}"
+          puts "last_open_character: #{last_open_character}"
+        end
         if last_open_character != matching_open_character
+          puts "false 2"
           return false
         end
       elsif is_open_character
-        if index == text.size - 1
-          return false
-        end
-        current_open_characters.push(character)
+        # if index == text.size - 1
+        puts "false 3"
+        #   return false
+        # end
+        open_stack.push(character)
+        open_count[character] += 1
       else
+        puts "false 4"
         return false
       end
+    end
+
+    # pp open_count.values
+    return open_count.values.all? { |x| x != 0 }
+    begin
+      
+    rescue => exception
+      pp open
     end
 
 
