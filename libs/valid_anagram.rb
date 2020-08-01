3f28cfb44f6370934a3f96a593ea81eb690299cc
Chris Aga <chrisaga.dev@gmail.com>
2020-08-01 15:36:12 -0500

    Finish anagram problem

--- libs/valid_anagram.rb
+++ libs/valid_anagram.rb
@@ -21,10 +21,72 @@
 
 class ValidAnagram
 
+  # ASCII_COUNTS = (?!...?~).reduce(:+).chars.map { |x| [x.ord, 0] }.to_h
+  
+  # attr_accessor :s_ord_count, :t_ord_count
+  
   # @param {String} s
   # @param {String} t
   # @return {Boolean}
+  # def set_up_ascii_characters
+  #   table = Array.new(256, 0)
+  # end
+
+
   def is_anagram(s, t)
-    nil
+    table = Array.new(256, 0)
+
+    s.each_char do |character|
+      table[character.ord] += 1
+    end
+
+    t.each_char do |character|
+      table[character.ord] -= 1
+      return false if table[character.ord] < 0
+    end
+
+    t.length == s.length
   end
+  
+  
+  # def is_anagram_2nd(s, t)
+  #   table = Array.new(256, 0)
+
+  #   ord_count(s, table) == ord_count(t, table)
+  # end
+  
+  
+  # def is_anagram_old(s, t)
+  #   # s_chars = s.chars.reject { |x| x == " " }
+  #   # t_chars = t.chars.reject { |x| x == " " }
+    
+  #   # s_chars.sort == t_chars.sort
+
+  #   @s_ord_count = old_ord_count(s)
+  #   @t_ord_count = old_ord_count(t)
+
+  #   @s_ord_count == @t_ord_count
+  # end
+
+  # private
+
+  # def ord_count(string, table)
+  #   table = table.clone
+  #   array_ords = string.each_char.each do |character|
+  #     table[character.ord] += 1
+  #   end
+
+  #   table
+  # end
+  
+  # def old_ord_count(string)
+  #   array = string.chars
+  #   array_ascii_counts = ASCII_COUNTS.clone
+  #   array_ords = array.map(&:ord)
+  #   array_ords.each do |ord_val|
+  #     array_ascii_counts[ord_val] += 1
+  #   end
+
+  #   array_ascii_counts
+  # end
 end
